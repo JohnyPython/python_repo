d84de1ca96e5ec7ab68e807de3ed0bed199eee90
JonnyNOdeep <89638552102es@gmail.com>
2022-01-07 21:44:42 +0500

    home_work#3

--- victory.py
+++ victory.py
@@ -22,25 +22,36 @@
 
 import random
 
-
 while True:
     famous_peoples = {'Ilon Mask': '28.06.1971', 'Sergei Brin': '21.08.1973', 'Jack Ma': '10.09.1964',
                       'Bruce Lee': '20.07.1940', 'Guido van Rossum': '31.01.1956', 'Magnus Carlsen': '30.11.1990',
                       'Maria Skłodowska-Curie': '07.11.1867', 'Martin Scorsese': '17.11.1942',
                       'Evgeny Kharlamov': '11.06.1985', 'Ekaterina Kharlamova': '11.10.1983'}
 
+    day_list = ['первго', 'второго', 'третьего', 'четвёртого', 'пятого', 'шестого', 'седьмого', 'восьмого',
+                'девятого', 'десятого', 'одиннадцатого', 'двенадцатого','тринадцатого', 'четырнадцатого', 'пятнадцатого',
+                'шестнадцатого','семнадцатого', 'восемнадцатого', 'девятнадцатого', 'двадцатого','двадцать первого',
+                'двадцать второго', 'двадцать третьго','двадацать четвёртого', 'двадцать пятого', 'двадцать шестого',
+                'двадцать седьмого', 'двадцать восьмого', 'двадцать девятого', 'тридцатого', 'тридцать первого']
+
+    month_list = ['января', 'февраля', 'марта', 'апреля', 'мая', 'июня',
+                  'июля', 'августа', 'сентября', 'октября', 'ноября', 'декабря']
+
     famous_peoples_keys_list = list(famous_peoples.keys())
 
-    random_five_names = random.sample(famous_peoples_keys_list, 5)
+    random_five_names = random.sample(famous_peoples_keys_list, 1)
     right_answer_count = 0
     wrong_answer_count = 0
     for i in random_five_names:
+        date = famous_peoples[i]
         input_birthday = input("Enter {}'s birthday(dd.mm.yyyy): ".format(i))
         if famous_peoples[i] == input_birthday:
             right_answer_count += 1
         else:
+            date_list = date.split('.')
+            right_answer = str(day_list[int(date_list[0]) - 1] + ' ' + month_list[int(date_list[1]) - 1] + ' ' + date_list[2] + ' года')
             wrong_answer_count += 1
-            print('Wrong answer, birthday of {} : {}'.format(i, famous_peoples[i]))
+            print("Your answer ist right\n{} was born {} : ".format(i, right_answer))
 
     print("Right answers: {}".format(right_answer_count))
     print("Wrong answers: {}".format(wrong_answer_count))
@@ -49,5 +60,3 @@ while True:
     if ask_about_play == 'no':
         print('Давай до свиданья!!!')
         break
-
-
